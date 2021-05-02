5e3c8f6e4ebfae0577f08f628e3f5bfe55c02822
Jay Shollenberger <jay@jayshollenberger.com>
2021-05-02 16:13:03 -0600

    Initial commit

---
+++ src/main/java/com/example/openshiftwarangular/OpenshiftWarAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.openshiftwarangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftWarAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshiftWarAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/openshiftwarangular/OpenshiftWarAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openshiftwarangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftWarAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
